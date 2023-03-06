e0e2e985579ca99e7de8bcc473d1a687ad84675a
MuhammadFahru <fahrumuhammad.ozi@gmail.com>
2023-03-06 17:57:41 +0700

    Upload

--- Python/Program/Dosen.py
+++ Python/Program/Dosen.py
@@ -9,18 +9,18 @@ class Dosen(SivitasAkademik):
     __pendidikan_terakhir = ""
     __keahlian = ""
 
-    def __init__(self, nik="", nama="", gender="", asal_universitas="", email_edu="", nim="", prodi = "", fakultas="", pendidikan_terakhir="", keahlian=""):
+    def __init__(self, nik="", nama="", gender="", asal_universitas="", email_edu="", nip="", prodi = "", fakultas="", pendidikan_terakhir="", keahlian=""):
         super().__init__(nik, nama, gender, asal_universitas, email_edu)
-        self.__nip = nim
+        self.__nip = nip
         self.__prodi = prodi
         self.__fakultas = fakultas
         self.__pendidikan_terakhir = pendidikan_terakhir
         self.__keahlian = keahlian
 
-    def set_nim(self, nim):
-        self.__nip = nim
+    def set_nip(self, nip):
+        self.__nip = nip
 
-    def get_nim(self):
+    def get_nip(self):
         return self.__nip
 
     def set_fakultas(self, fakultas):
